6ce763ef6ca584ca5dbbae021f1174bbde765b8b
thuctapgit <vinhnt62@wru.vn>
2020-09-22 17:21:25 +0700

    seach post by category, content, title

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -24,8 +24,8 @@
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
                         Đăng bài viết
                     </button>
-                    
-                    
+
+
                     <!-- Modal -->
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
                         aria-hidden="true">
@@ -38,43 +38,46 @@
                                     </button>
                                 </div>
                                 {{-- Start Popup --}}
-                                <form name="Mypost" action="Post" method="POST" onsubmit="return validate()">
-                                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                                <form id="Mypost" name="Mypost" action="Post" method="POST"
+                                    onsubmit="return validate()">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                     <div class="modal-body">
 
-                                    <!-- Category -->
-                                    {{-- <div class="mb-3"> --}}
-                                        <select class="custom-select mr-sm-2"style="color:blue" id="category" name="category">
-                                            
+                                        <!-- Category -->
+                                        {{-- <div class="mb-3"> --}}
+                                        <select class="custom-select mr-sm-2" style="color:blue" id="category"
+                                            name="category">
+
                                             <option selected value="0">Chọn Thể Loại</option>
                                             @foreach($category as $tl)
-                                                <option value="{{ $tl->id }}">{{ $tl->title }}</option> 
+                                                <option value="{{ $tl->id }}">{{ $tl->title }}</option>
                                             @endforeach
-
-                                            
                                         </select>
-                                        
-                                    {{-- </div> --}}
-                                        
-                                    <!-- end  -->
+
+                                        {{-- </div> --}}
+
+                                        <!-- end  -->
                                         <div class="form-group">
                                             <label class="textmodal" style="color:blue">Tiêu đề</label>
-                                            <input type="text" class="form-control" id="title" name="title" required>
+                                            <input type="text" class="form-control" id="title" name="title">
+
                                             <span id="titleloc"></span>
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleFormControlTextarea1" style="color:blue">Nội dung</label>
-                                            <textarea class="form-control" id="exampleFormControlTextarea1"
-                                                rows="3" name="content"></textarea>
-                                                <span id="show-error"></span>
+                                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                                name="content"></textarea>
+                                            <span id="show-error"></span>
                                         </div>
 
 
                                     </div>
-                                <!-- ENDPOPUP -->
+                                    <!-- ENDPOPUP -->
                                     <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        <button id="btn-save" type="submit" class="btn btn-primary">Save changes</button>
+                                        <button type="button" class="btn btn-secondary"
+                                            data-dismiss="modal">Close</button>
+                                        <button id="btn-save" type="submit" class="btn btn-primary">Save
+                                            changes</button>
                                     </div>
                                 </form>
 
@@ -83,19 +86,24 @@
                     </div>
                     <!-- End Modal -->
                 </div>
-                
+
                 @if(session('thongbao'))
                     <div class="alert alert-success">
                         {{ session('thongbao') }}
                     </div>
                 @endif
-                
+
                 @foreach($post as $pt)
-                <hr>
-                <h4>Vmo Confession</h4>
+                    <hr>
+                    <h4>Vmo Confession</h4>
                     <p>title: {{ $pt->title }}</p>
                     <p>{{ $pt->content }}</p>
                     <!-- con -->
+                    <!-- <hr>
+                <p>2 like</p>
+                <hr> -->
+                    
+                    
                     <button type="button" class="btn btn-primary" id="like">
                         <span class="badge badge-light">Like</span>
                         <span class="sr-only">unread messages</span>
@@ -106,10 +114,10 @@
                     </button>
                     <button type="button" class="btn btn-primary" id="like">
                         <span class="badge badge-light">Comment</span>
-                        <span class="sr-only">unread messages</span>
+
                     </button>
                 @endforeach
-                
+
             </div>
 
         </div>

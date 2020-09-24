4b8a4292778e856a86363c1d32846215f8243750
thuctapgit <vinhnt62@wru.vn>
2020-09-24 14:36:33 +0700

    rate

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -32,7 +32,8 @@
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title textmodal" id="exampleModalLabel" style="color:blue">Đăng bài</h5>
+                                    <h5 class="modal-title textmodal" id="exampleModalLabel" style="color:blue">Đăng bài
+                                    </h5>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
@@ -101,20 +102,64 @@
                     <!-- <hr>
                 <p>2 like</p>
                 <hr> -->
-                    
-                    
-                    <button type="button" class="btn btn-primary" id="like">
-                        <span class="badge badge-light">Like</span>
-                        <span class="sr-only">unread messages</span>
-                    </button>
-                    <button type="button" class="btn btn-primary" id="like">
-                        <span class="badge badge-light">Dislike</span>
-                        <span class="sr-only">unread messages</span>
-                    </button>
-                    <button type="button" class="btn btn-primary" id="like">
-                        <span class="badge badge-light">Comment</span>
 
-                    </button>
+
+                    <div>
+                        <button type="button" class="btn btn-primary" id="like">
+                            <span class="badge badge-light">Like</span>
+                            <span class="sr-only">unread messages</span>
+                        </button>
+                        <button type="button" class="btn btn-primary" id="like">
+                            <span class="badge badge-light">Dislike</span>
+                            <span class="sr-only">unread messages</span>
+                        </button>
+                        <button type="button" class="btn btn-primary" id="like">
+                            <span class="badge badge-light">Comment</span>
+                        </button>
+
+                        <!-- RATING -->
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
+                            Đánh giá
+                        </button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false"
+                            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="staticBackdropLabel">Đánh giá</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <form action="rate" method="POST">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <div class="modal-body">
+                                            <div class="container">
+                                                
+
+                                                <h5>Click to rate:</h5>
+                                                <div class='starrr' id='star1'></div>
+                                                <div>&nbsp;
+                                                    <span class='your-choice-was' style='display: none;'>
+                                                        Your rating was <span class='choice'></span>.
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">Close</button>
+                                                <button type="submit" class="btn btn-primary">Gửi</button>
+                                            </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- END RATING -->
+                    </div>
+
                 @endforeach
 
             </div>
